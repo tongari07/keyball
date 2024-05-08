f0190f04ef00ce4e28f275b5ff87bc8aa8adf59d
tongari07 <togawa.naoto@moneyforward.co.jp>
2024-05-08 17:08:29 +0900

    Remove

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/tongari07/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/tongari07/keymap.c
@@ -71,6 +71,5 @@ void oledkit_render_info_user(void) {
 #endif
 
 void pointing_device_init_user(void) {
-    set_auto_mouse_layer(<mouse_layer>); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
 }
